1813c6c97752e5ca086de3453b8a104847212c9b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:30 -0500

    TUnit conversion

--- test/Soenneker.Quark.Gen.Lucide.Abstractions.Tests/ILucideIconSvgProviderTests.cs
+++ test/Soenneker.Quark.Gen.Lucide.Abstractions.Tests/ILucideIconSvgProviderTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Gen.Lucide.Abstractions.Tests;
 
 public sealed class ILucideIconSvgProviderTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
